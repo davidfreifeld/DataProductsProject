504c5485877326a00957bab54c841352e093073f
David Freifeld <david.freifeld@gmail.com>
2015-05-23 20:33:18 -0400

    one last touch...go rangers

--- ui.R
+++ ui.R
@@ -15,7 +15,8 @@ shinyUI(pageWithSidebar(
           To use this tool, the user should first select which of the four stats (above)
           that he would like to include in the linear model. Then he can get a prediction
           using this model by inputting a value for each of the statistics selected. The
-          predicted number of points will appear at the top on the right.')
+          predicted number of points will appear at the top on the right. Note that the
+          default values for each stat are those of the first place New York Rangers.')
     ),
 
     mainPanel(
